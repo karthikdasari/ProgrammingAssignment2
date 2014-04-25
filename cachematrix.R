e8d1d69066fb90a31369442fb7441cf15b6931cb
karthikdasari <karthik.dasari@myra.ac.in>
2014-04-25 16:23:30 -0700

    Programming Assignment 2
    Programming Assignment 2 for peer review

--- cachematrix.R
+++ cachematrix.R
@@ -2,15 +2,37 @@
 ## cacheSolve computes the inverse of a matrix or returns the   
 ## cached value if the calculation is alredy done 
 
-## Write a short comment describing this function
+## makeCacheMatrix returns a list of four functions set, get    ##setinverse and getinverse
 
 makeCacheMatrix <- function(x = matrix()) {
+	I <- NULL
+        set <- function(y) {
+                x <<- y
+                I <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(Inverse) I <<- Inverse
+        getInverse <- function() I
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)	
 
 }
 
 
-## Write a short comment describing this function
+## This function looks in the element that is passed as an    argument      ##and retrieves the inverse of the original matrix from the cache, 
+##or, calculates it and stores it accordingly 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      I <- x$getInverse()
+        if(!is.null(I)) {
+                message("getting cached data")
+                return(I)
+        }
+     matrix <- x$get()
+     library(MASS)
+       I <- ginv(matrix)  #ginv in the MASS package will give the generalized inverse of a matrix      
+       x$setInverse(I)
+       I
+
 }
